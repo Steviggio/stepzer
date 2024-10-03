e82a60c506cf411d7853aa9fb9e2e71a1710239b
seb_grtd <sebastien.gratade@etu.univ-orleans.fr>
2024-10-03 11:49:47 +0200

    accueil valeurs par défaut

--- app/home/home.tsx
+++ app/home/home.tsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Text, List, ListItem, Layout } from '@ui-kitten/components';
 import { supabase } from '@/lib/supabase';
-import GoogleFit, { Scopes } from 'react-native-google-fit';
 
 export default function Home() {
   const [email, setEmail] = useState<string | null>(null);
-  const [steps, setSteps] = useState<number | null>(null);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -17,67 +16,51 @@ export default function Home() {
       }
     };
 
-    console.log('\n\n FETCHING STEPS \n\n')
-
-    GoogleFit.checkIsAuthorized().then(() => {
-      console.log(GoogleFit.isEnabled) // Then you can simply refer to `GoogleFit.isAuthorized` boolean.
-  })
-
-    const fetchSteps = async () => {
-      const options = {
-        scopes: [
-          Scopes.FITNESS_ACTIVITY_READ,
-          Scopes.FITNESS_ACTIVITY_WRITE,
-        ],
-      };
-
-      GoogleFit.authorize(options)
-        .then(authResult => {
-          if (authResult.success) {
-            console.log('Authorization successful');
-            const today = new Date();
-            const options = {
-              startDate: today.setHours(0, 0, 0, 0).toISOString(), // Min time for today
-              endDate: new Date().toISOString(), // Max time for today
-            };
-
-            GoogleFit.getDailyStepCountSamples(options).then(res => {
-              console.log('Step count samples:', res);
-              if (res.length > 0) {
-                const stepsToday = res[0].steps.reduce((total, step) => total + step.value, 0);
-                setSteps(stepsToday);
-              } else {
-                console.log('No step count samples available');
-              }
-            }).catch(error => {
-              console.error('Error fetching step count samples:', error);
-            });
-          } else {
-            console.error('AUTH_DENIED', authResult.message);
-          }
-        })
-        .catch(error => {
-          console.error('AUTH_ERROR', error);
-        });
-    };
-
     fetchUser();
-    fetchSteps();
   }, []);
 
+  const friends = [
+    {
+      name: 'mickael',
+      steps: 10000
+    },
+    {
+      name: 'julien',
+      steps: 5000
+    },
+    {
+      name: 'steve',
+      steps: 2000
+    }
+  ];
+
+  const renderItem = ({ item }: { item: { name: string; steps: number } }) => (
+    <ListItem
+      title={`${item.name}`}
+      description={`Steps: ${item.steps}`}
+    />
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>home</Text>
+    <Layout style={styles.container}>
+      <Text category='h1'>Home</Text>
       <Text>{email ? `Email: ${email}` : 'No user logged in'}</Text>
-      <Text>{steps !== null ? `Nombre de pas aujourd'hui : ${steps}` : 'Pas de données de pas disponibles'}</Text>
-    </View>
+      <List
+        data={friends}
+        renderItem={renderItem}
+        style={styles.list}
+      />
+    </Layout>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 16,
+  },
+  list: {
+    marginTop: 16,
+    width: '100%',
   },
 });
