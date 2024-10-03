749cdcec2eab385b3e7cb58f250219216cdc469a
araviart <alexandre.raviart@etu.univ-orleans.fr>
2024-10-03 12:01:47 +0200

    appelle a un edge function qui fetch les users

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,89 +1,42 @@
 import React, { useState } from 'react';
-import { Alert, StyleSheet, View } from 'react-native';
+import { View, Button, TextInput, Alert } from 'react-native';
 import { supabase } from '../lib/supabase';
-import { Button, Input } from '@rneui/themed';
+import { useRouter } from 'expo-router'; 
 
 export default function Auth() {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-  const [loading, setLoading] = useState<boolean>(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
-  async function signInWithEmail() {
+  const signIn = async () => {
     setLoading(true);
-    const { data: { session }, error } = await supabase.auth.signInWithPassword({
-      email: email,
-      password: password,
-    });
+    const { error } = await supabase.auth.signInWithPassword({ email, password });
 
     if (error) {
       Alert.alert(error.message);
-      console.log('Sign in failed:', error.message);
     } else {
-      console.log('User signed in:', session);
+      console.log('dashboardpush')
+      router.push('/dashboard'); 
     }
-    setLoading(false);
-  }
 
-  async function signUpWithEmail() {
-    setLoading(true);
-    const { data: { session }, error } = await supabase.auth.signUp({
-      email: email,
-      password: password,
-    });
-
-    if (error) {
-      Alert.alert(error.message);
-      console.log('Sign up failed:', error.message);
-    } else {
-      console.log('User signed up:', session);
-    }
     setLoading(false);
-  }
+  };
 
   return (
-    <View style={styles.container}>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Input
-          label="Email"
-          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-          onChangeText={(text) => setEmail(text)}
-          value={email}
-          placeholder="email@address.com"
-          autoCapitalize={'none'}
-        />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Input
-          label="Password"
-          leftIcon={{ type: 'font-awesome', name: 'lock' }}
-          onChangeText={(text) => setPassword(text)}
-          value={password}
-          secureTextEntry={true}
-          placeholder="Password"
-          autoCapitalize={'none'}
-        />
-      </View>
-      <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button title="Sign in" disabled={loading} onPress={signInWithEmail} />
-      </View>
-      <View style={styles.verticallySpaced}>
-        <Button title="Sign up" disabled={loading} onPress={signUpWithEmail} />
-      </View>
+    <View>
+      <TextInput
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+      />
+      <TextInput
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+      />
+      <Button onPress={signIn} title={loading ? 'Signing In...' : 'Sign In'} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 12,
-  },
-  verticallySpaced: {
-    paddingTop: 4,
-    paddingBottom: 4,
-    alignSelf: 'stretch',
-  },
-  mt20: {
-    marginTop: 20,
-  },
-});
